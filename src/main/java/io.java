c0df69526c977226b4b39b4bb065726f3077b4df
Moderocky <admin@moderocky.com>
2022-09-20 12:55:35 +0100

    Add basic functions.

---
+++ src/main/java/io.java
@@ -0,0 +1,78 @@
+import mx.kenzie.jupiter.stream.InputStreamController;
+import mx.kenzie.jupiter.stream.Stream;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Iterator;
+
+/**
+ * This is the Java implementation of the 'io' namespace functions.
+ * Some implementations may re-implement this in Skript itself.
+ * <p>
+ * The unusual casing is to match Skript's code-style.
+ */
+public final class io {
+    
+    public static OutputStream open_output_stream(Object object) throws IOException {
+        if (object == null) return null;
+        if (object instanceof OutputStream stream) return stream;
+        if (object instanceof File file) return new FileOutputStream(file);
+        return null;
+    }
+    
+    public static Iterable<Byte> loop_bytes(Object object) throws IOException {
+        if (object instanceof InputStreamController controller) return controller;
+        if (object instanceof InputStream stream) return Stream.controller(stream);
+        if (object == null) return () -> new Iterator<>() {
+            @Override
+            public boolean hasNext() {
+                return false;
+            }
+            
+            @Override
+            public Byte next() {
+                return -1;
+            }
+        };
+        return Stream.controller(open_input_stream(object));
+    }
+    
+    public static InputStream open_input_stream(Object object) throws IOException {
+        if (object == null) return null;
+        if (object instanceof InputStreamController stream) return stream;
+        if (object instanceof InputStream stream) return stream;
+        if (object instanceof File file) return new FileInputStream(file);
+        if (object instanceof String string) return new ByteArrayInputStream(string.getBytes(StandardCharsets.UTF_8));
+        return new ByteArrayInputStream(new byte[0]);
+    }
+    
+    public static void close(Object object) throws Throwable {
+        if (object instanceof InputStreamController controller) controller.close();
+        if (object instanceof AutoCloseable closeable) closeable.close();
+    }
+    
+    public static Byte read_byte(InputStream stream) throws IOException {
+        return (byte) stream.read();
+    }
+    
+    public static String read_character(InputStream stream) throws IOException {
+        final int first = stream.read(), second = stream.read();
+        if (second < 0) throw new EOFException();
+        return "" + (char) ((first << 8) + second);
+    }
+    
+    public static Integer read_integer(InputStream stream) throws IOException {
+        final int a = stream.read(), b = stream.read(), c = stream.read(), d = stream.read();
+        if (d < 0) throw new EOFException();
+        return (a << 24) + (b << 16) + (c << 8) + d;
+    }
+    
+    public static String read_string(InputStream stream, Number length) throws IOException {
+        final int amount = length.intValue();
+        final byte[] bytes = new byte[amount];
+        final int count = stream.read(bytes);
+        assert count == amount;
+        return new String(bytes, StandardCharsets.UTF_8);
+    }
+    
+}
---
+++ src/main/java/org/byteskript/io/Main.java
@@ -0,0 +1,53 @@
+package org.byteskript.io;
+
+import mx.kenzie.foundation.Type;
+import mx.kenzie.foundation.language.PostCompileClass;
+import org.byteskript.skript.api.ModifiableLibrary;
+import org.byteskript.skript.runtime.Skript;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Collection;
+import java.util.List;
+
+public class Main extends ModifiableLibrary {
+    public static final Main LIBRARY = new Main();
+    public static final Type BYTE = new Type(Byte.class),
+        INPUT = new Type(InputStream.class),
+        OUTPUT = new Type(OutputStream.class);
+    
+    public Main() {
+        super("io");
+        this.registerTypes(BYTE, INPUT, OUTPUT);
+    }
+    
+    public static void main(String... args) {
+        System.out.println("This goes in the ByteSkript libraries/ folder.");
+    }
+    
+    public static void load(Skript skript) {
+        skript.registerLibrary(LIBRARY);
+    }
+    
+    @Override
+    public Collection<PostCompileClass> getRuntime() {
+        try {
+            return List.of(this.getData(Class.forName("io")));
+        } catch (ClassNotFoundException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    private PostCompileClass getData(final Class<?> type) throws IOException {
+        return new PostCompileClass(getSource(type), type.getName(), new Type(type).internalName());
+    }
+    
+    private byte[] getSource(final Class<?> cls) throws IOException {
+        try (final InputStream stream = ClassLoader.getSystemResourceAsStream(cls.getName()
+            .replace('.', '/') + ".class")) {
+            assert stream != null;
+            return stream.readAllBytes();
+        }
+    }
+}
